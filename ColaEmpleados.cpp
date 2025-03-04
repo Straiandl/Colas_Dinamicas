e8bb0bc75f7400470854e0a9f550d0cd70dfa4f2
Straiandl <123585133+Straiandl@users.noreply.github.com>
2025-03-03 20:50:57 -0600

    Update ColaEmpleados.cpp

--- ColaEmpleados.cpp
+++ ColaEmpleados.cpp
@@ -1,78 +1,74 @@
 //De la Paz Mendoza Ian ALexandro. 25A.
-//Clases y objetos.
+//Colas dinamicas.
 
 #include <iostream>
 #include <string>
 const int Tam=50;
 
-class empleado {
+class alumno {
 private: //Atributos deben de ir aqui
-    int ClaveEmpleado;
     std::string nombre;
-    std::string domicilio;
-    float sueldo;
-    std::string ReportaA;
+    std::string carrera;
+    int aprovadas;
+    float promedio;
 public:
-    empleado() { ClaveEmpleado = 0; nombre = " "; domicilio = " "; sueldo = 0; ReportaA = " "; } //constructor
+    alumno() { nombre = " "; carrera = " "; aprovadas = 0; promedio = 0.0; } //constructor
 
-    void setClaveE(int clave) { ClaveEmpleado = clave; }
-    void setnombre(std::string nom) { nombre = nom; }
-    void setdomicilio(std::string dom) { domicilio = dom; }
-    void setsueldo(float s) { sueldo = s; }
-    void setReporta(std::string reporta) { ReportaA = reporta; }
-
-    float getsueldo(){
-        return sueldo;
-    }
+    void setNombre(int nom) { nombre = nom; }
+    void setCarrera(std::string carr) {carrera = carr; }
+    void setAprovadas(int apro) { aprovadas = apro; }
+    void setpromedio(float prom) { promedio = prom; }
 
 
     // sobrecarga de operadores.
-    friend std::ostream& operator<<(std::ostream& os, const empleado& emp);
+    friend std::ostream& operator<<(std::ostream& os, const alumno& emp);
 
-    friend std::istream& operator>>(std::istream& is, empleado& emp);
+    friend std::istream& operator>>(std::istream& is, alumno& emp);
 
 
 
-    bool operator==(const empleado& otro) const {
-        return sueldo == otro.sueldo;
+    bool operator==(const alumno& otro) const {
+        return promedio == otro.promedio;
     }
 
-    bool operator > (const empleado& otro) const {
-        return sueldo > otro.sueldo;
+    bool operator > (const alumno& otro) const {
+        return promedio > otro.promedio;
     }
 
-    bool operator < (const empleado& otro) const{
-        return sueldo < otro.sueldo;
+    bool operator < (const alumno& otro) const{
+        return promedio < otro.promedio;
     }
 
-    bool operator != (const empleado& otro) const{
-        return sueldo != otro.sueldo;
+    bool operator != (const alumno& otro) const{
+        return promedio != otro.promedio;
     }
 
-    empleado operator + (const empleado& otro) const {
-        empleado temp;
-        temp.sueldo = this ->sueldo + otro.sueldo;
+    alumno operator + (const alumno& otro) const {
+        alumno temp;
+        temp.promedio = this ->promedio + otro.promedio;
         return temp;
     }
 
 
 };
 
-class PilaEmpleado {
+
+
+class ColaEmpleado {
     private:
-        empleado datos[Tam]; // Arreglo de objetos empleado
-        int ult; // Índice del último elemento
+        alumno datos[Tam];
+        int frente, final;
     public:
-        PilaEmpleado() : ult(-1) {}
+        ColaEmpleado() : frente(0), final(-1) {}
     
-        bool vacia() const { return ult == -1; }
-        bool llena() const { return ult == Tam - 1; }
+        bool vacia() const { return final < frente; }
+        bool llena() const { return final == Tam - 1; }
     
-        void insertar(const empleado& emp) {
+        void insertar(const alumno& emp) {
             if (llena()) {
                 std::cout << "\nError: Pila llena." << std::endl;
             } else {
-                datos[++ult] = emp; // Inserta el empleado en la pila
+                datos[++final] = emp;
             }
         }
     
@@ -80,84 +76,85 @@ class PilaEmpleado {
             if (vacia()) {
                 std::cout << "\nError: Pila vacía." << std::endl;
             } else {
-                --ult; // Elimina el último elemento
+                frente++;
             }
         }
     
-        empleado recuperar() const {
+        alumno recuperar() const {
             if (vacia()) {
                 std::cout << "\nError: Pila vacía." << std::endl;
-                return empleado(); // Retorna un empleado vacío
+                return alumno();
             } else {
-                return datos[ult]; // Retorna el último elemento
+                return datos[frente];
             }
         }
-    
-        int ultimo() const {
-            return ult;
+
+        void mostrar () const {
+            if (vacia()){
+                std::cout << "\nCola vacia." << std::endl;
+
+            }
+            else{
+                for (int i = frente; i <= final; i++){
+                    std::cout << datos[i] << std::endl;
+                }
+            }
         }
     };
     
 
-std::ostream& operator<<(std::ostream& os, const empleado& emp){
-    os << "\nClave de empleado: " << emp.ClaveEmpleado << std::endl;
-    os << "Nombre: " << emp.nombre << std::endl;
-    os << "Domicilio: " << emp.domicilio << std::endl;
-    os << "Sueldo: " << emp.sueldo << std::endl;
-    os << "Reporta a: " << emp.ReportaA << std::endl;
+std::ostream& operator<<(std::ostream& os, const alumno& emp){
+    os << "\nNombre del alumno: " << emp.nombre << std::endl;
+    os << "Nombre de la materia: " << emp.carrera << std::endl;
+    os << "Materias aprovadas: " << emp.aprovadas << std::endl;
+    os << "Promedio: " << emp.promedio << std::endl;
     return os;
 }
 
-std::istream& operator>>(std::istream& is, empleado& emp) {
-    std::cout << "Introduce la clave de empleado: ";
-    is >> emp.ClaveEmpleado;
-
-    std::cout << "Introduce el nombre: ";
+std::istream& operator>>(std::istream& is, alumno& emp) {
+    std::cout << "Introduce el nombre del alumno: ";
     is >> emp.nombre;
 
-    std::cout << "Introduce el domicilio: ";
-    is >> emp.domicilio;
+    std::cout << "Introduce el nombre de la materia: ";
+    is >> emp.carrera;
 
-    std::cout << "Introduce el sueldo: ";
-    is >> emp.sueldo;
+    std::cout << "Introduce las materias aprovadas: ";
+    is >> emp.aprovadas;
 
-    std::cout << "reporte: ";
-    is >> emp.ReportaA;
+    std::cout << "Introduce el promedio: ";
+    is >> emp.promedio;
 
     return is;
 }
 
-int main()
+int main()//Mipila
 {
-    PilaEmpleado Mipila;
-    empleado JefePlanta, JefePersonal, emp;
+    ColaEmpleado Micola;
+    alumno JefePlanta, JefePersonal, emp;
     int op=0, num;
-    while (op != 10) {
-        std::cout << "\n Bienvenido \n elige una opcion: \n 1: Registrar empleados. \n 2: Mostrar los empleados. \n 3: Eliminar el ultimo empleado. \n 4: Mostrar el ultimo empleado. \n 5. Presiona 5 para salir. \n \n";
+    while (op != 5) {
+        std::cout << "\n \nBienvenido\nElige una opcion:\n1: Registrar alumnos.\n2: Mostrar todos los alumnos.\n3: Eliminar el ultimo alumno.\n4: Mostrar el ultimo alumno.\n5: Salir.\n\n";
         std::cin >> op;
         switch (op){
             case 1:
-            std::cout << "Cuantos empleados quieres registrar? ";
+            std::cout << "Cuantos alumnos quieres registrar? ";
             std::cin >> num;
                 for (int i = 0; i < num; i++) {
-                    std::cout << "\nIntroduce los datos del empleado " << i + 1 << ":\n";
+                    std::cout << "\nIntroduce los datos del alumno " << i + 1 << ":\n";
                     std::cin >> emp;
-                    Mipila.insertar(emp); 
+                    Micola.insertar(emp); 
                 }
                 break;
             case 2:
-                std::cout << "\n Empleados registrados:";
-                while (!Mipila.vacia()) {
-                    std::cout << Mipila.recuperar() << std::endl;
-                    Mipila.eliminar(); 
-                }
+                std::cout << "\n Alumnos registrados:";
+                Micola.mostrar();
                 break;
             case 3:
-            std::cout << "\n Empleado eliminado.\n";
-            Mipila.eliminar();
+            std::cout << "\n Alumno eliminado.\n";
+            Micola.eliminar();
                 break;
             case 4:
-                std::cout << "\nEl ultimo empleado registrado:" << Mipila.recuperar() << std::endl;
+                std::cout << "\nEl ultimo alumno registrado:" << Micola.recuperar() << std::endl;
                 break;
             case 5:
                 std::cout << "\nGracias\n";
